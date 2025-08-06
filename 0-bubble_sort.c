b1b07bc203c6cbe1a365381d38a7394aba478ab0
Boubouche1709 <hadjarflorian@gmail.com>
2025-08-06 09:52:13 +0200

    fix betty style file 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -25,16 +25,14 @@ void bubble_sort(int *array, size_t size)
 size_t i, j;
 
 for (i = 0; i < size - 1; i++)
-	{
-		for (j = 0; j < size - 1 - i; j++)
-		{
-			if (array[j] > array[j + 1])
-			{
-				swap(&array[j], &array[j + 1]);
-				print_array(array, size);
-			}
-		}
-
-	}
-
+{
+for (j = 0; j < size - 1 - i; j++)
+{
+if (array[j] > array[j + 1])
+{
+swap(&array[j], &array[j + 1]);
+print_array(array, size);
+}
+}
+}
 }
