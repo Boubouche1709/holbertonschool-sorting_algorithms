f9adb6bd81453a417a556b5c32fbd18bc6860906
Boubouche1709 <hadjarflorian@gmail.com>
2025-08-07 14:17:09 +0200

    fix some trouble in lomuto

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -26,7 +26,7 @@ int lomuto_partition(int *array, size_t size, int low, int high)
 int pivot = array[high];
 int i = (low - 1);
 int j = 0;
-for (j = low; j < high - 1; j++)
+for (j = low; j < high; j++)
 {
 if (array[j] <= pivot)
 {
@@ -55,10 +55,9 @@ return (i + 1);
 */
 void quick_sort_recursive(int *array, size_t size, int low, int high)
 {
-int pivot_index;
 if (low < high)
 {
-pivot_index = lomuto_partition(array, size, low, high);
+int pivot_index = lomuto_partition(array, size, low, high);
 quick_sort_recursive(array, size, low, pivot_index - 1);
 quick_sort_recursive(array, size, pivot_index + 1, high);
 }
